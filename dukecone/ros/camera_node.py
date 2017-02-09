e6fb02ec2cb49c4b32eae4d48e9c0ef41f59a9ca
mfm6 <melissafm24@gmail.com>
2017-02-09 14:13:04 -0500

    ROS msg conversion to opencv rgb and depth image

--- dukecone/ros/camera_node.py
+++ dukecone/ros/camera_node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import cv2
-
+import numpy as np
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -9,20 +9,37 @@ from cv_bridge import CvBridge, CvBridgeError
 class ImageViewer(object):
     def __init__(self):
         self.bridge = CvBridge()
-        self.image_topic = "/camera/rgb/image_color"
-        self.image_sub = rospy.Subscriber(self.image_topic,
-                                          Image,
-                                          self.callback)
+        self.image_rgb = "/camera/rgb/image_color"
+        self.image_depth = "/camera/depth_registered/image"
+        self.rgb_image_sub = rospy.Subscriber(self.image_rgb,
+                                              Image,
+                                              self.rgb_callback)
+        self.depth_image_sub = rospy.Subscriber(self.image_depth,
+                                                Image,
+                                                self.depth_callback)
 
-    def callback(self, data):
+    def rgb_callback(self, data):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            cv_rgb_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
         except CvBridgeError as e:
             print(e)
 
-        print cv_image
-        cv2.imshow("Image", cv_image)
+        cv2.imshow("RgbImage", cv_rgb_image)
+        cv2.imwrite("rgb_image.jpg", cv_rgb_image)
         cv2.waitKey(1)
+
+    def depth_callback(self, data):
+        try:
+            cv_depth_image = self.bridge.imgmsg_to_cv2(data, "8UC1")
+        except CvBridgeError as e:
+            print(e)
+
+        normalized = np.copy(cv_depth_image)
+        cv2.normalize(normalized, cv_depth_image, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+        cv2.imshow("Depth Image", cv_depth_image)
+        cv2.imwrite("gray_image.jpg", cv_depth_image)
+        cv2.waitKey(1)
+
 
 
 if __name__ == "__main__":
