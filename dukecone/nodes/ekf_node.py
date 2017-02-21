e8376aa35ecb36b0ce805e22823e34a6eeddffb8
Nav Ganti <navganti@github.com>
2017-02-21 17:54:56 -0500

    Added in rospy init node, modified topic names

--- dukecone/nodes/ekf_node.py
+++ dukecone/nodes/ekf_node.py
@@ -8,28 +8,29 @@ from geometry_msgs.msg import Twist
 from dukecone.msg import ObjectLocation
 from math import radians, pi
 
+
 class EKFNode():
 
     def __init__(self):
+        # Initialize ROS Node
+        rospy.init_node('EKFNode', anonymous=True)
 
         self.ekf = ekf.EKF()
 
-        bot_cmd_topic = 'cmd_vel_mux/input/navi'
+        bot_cmd_topic = '/cmd_vel_mux/input/navi'
         self.ekf_sub_input = rospy.Subscriber(
                                              bot_cmd_topic,
                                              Twist,
                                              self.bot_input_callback)
-        tf_obj_topic = 'tensorflow/object/location'
+        tf_obj_topic = '/tensorflow/object/location'
         self.ekf_sub_obj = rospy.Subscriber(tf_obj_topic,
                                             ObjectLocation,
                                             self.obj_callback)
 
-
-    def bot_input_callback(self):
+    def bot_input_callback(self, data):
         pass
 
-
-    def obj_callback(self):
+    def obj_callback(self, data):
         pass
 
 
@@ -40,4 +41,4 @@ if __name__ == '__main__':
     try:
         rospy.spin()
     except KeyboardInterrupt:
-        print("bye bye")
+        print("Shutting Down")
